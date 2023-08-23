f53a1b5d0be2d4017112974cbec7d671659e4c43
karmadosha <carmado@yandex.ru>
2023-08-23 22:23:28 +0800

    port

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,4 @@
-const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
+const { PORT = 4000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
 const { ORIGIN } = ['https://karmamovies.nomoredomainsicu.ru', 'http://karmamovies.nomoredomainsicu.ru', 'http://localhost:3001'];
 
